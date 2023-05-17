9707b2d40c05c5f97d6e60eb9cdd46004826bed7
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-18 01:02:11 +0300

    Create route.js

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,48 @@
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      sessionUser.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        // check if user exists
+        const userExist = await User.findOne({
+          email: profile.email,
+        });
+        // if not , create user
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
