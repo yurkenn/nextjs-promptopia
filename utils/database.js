54a9b19ad7bd86e9275e980241cd6681e11ff11c
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-18 01:02:46 +0300

    Create database.js

---
+++ utils/database.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // tract the connection
+
+export const connectToDB = async () => {
+  if (isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'share_prompt',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log('MongoDB is connected');
+  } catch (error) {
+    console.log(error);
+  }
+};
