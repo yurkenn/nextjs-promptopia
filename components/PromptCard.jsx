5bc4ff12e435988475f410aef9f6b687b67a3205
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-18 03:01:38 +0300

    Update PromptCard.jsx

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+'use client';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState('');
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={session?.user?.image}
+            alt="user_profile_picture"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">{session?.user?.name}</h3>
+            <p className="font-inter text-sm text-gray-500">{session?.user?.email}</p>
+          </div>
+        </div>
+        <div className="copy_btn" onClick={() => {}}>
+          <Image
+            src={copied === post.prompt ? './assets/icons/tick.svg' : './assets/icons/copy.svg'}
+            alt="copy"
+            width={16}
+            height={16}
+          />
+        </div>
+      </div>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
+    </div>
+  );
 };
 
 export default PromptCard;
