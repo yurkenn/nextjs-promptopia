d395fcb3e5ac17e3464715a8231602e978bebeba
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-21 23:11:08 +0300

    Update page.jsx

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,24 +3,44 @@ import React, { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Profile from '@components/Profile';
+import { Router } from 'next/router';
 
 const MyProfile = () => {
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      console.log(res);
       const data = await res.json();
       setPosts(data);
+      console.log(data);
     };
 
     if (session?.user.id) fetchPosts();
   }, [session?.user.id]);
 
-  const handleDelete = () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-  const handleEdit = () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm('Are you sure you want to delete this prompt?');
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
