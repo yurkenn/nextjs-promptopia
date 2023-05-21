d70a4a4415b68a0d1221e334be80cf36d57f5e5d
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-05-21 23:11:16 +0300

    Update PromptCard.jsx

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,19 +1,21 @@
 'use client';
 
-import { data } from 'autoprefixer';
+import { useState } from 'react';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-  const [copied, setCopied] = useState('');
   const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const [copied, setCopied] = useState('');
 
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setCopied(''), 3000);
+    setTimeout(() => setCopied(false), 3000);
   };
 
   return (
@@ -21,15 +23,15 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
-            src={session?.user?.image}
+            src={post.creator.image}
             alt="user_profile_picture"
             width={40}
             height={40}
             className="rounded-full object-contain"
           />
           <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold text-gray-900">{session?.user?.name}</h3>
-            <p className="font-inter text-sm text-gray-500">{session?.user?.email}</p>
+            <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
+            <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
           </div>
         </div>
         <div className="copy_btn" onClick={handleCopy}>
@@ -49,6 +51,16 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
+            Edit
+          </p>
+          <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
